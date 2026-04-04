fdbc1210dbd84f8f242ddda5aa10fc3f2e6bdbd9
bishnumahato219 <bishnumahato219@gmail.com>
2026-04-05 04:59:55 +0530

    Allow Vercel frontend in CORS

--- mahato-motors-backend/index.js
+++ mahato-motors-backend/index.js
@@ -13,7 +13,10 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173", // React default Vite port
+  origin: ""[
+    "http://localhost:5173",
+    "https://inventory-management-system-c2at38hkq.vercel.app"
+  ], // React default Vite port
   credentials: true
 }));
 
